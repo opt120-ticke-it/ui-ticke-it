5607e211d5e4f670be7aa0884b08ac4adc80220c
akiwnl <carvalhokaike69@gmail.com>
2024-12-18 18:22:49 -0300

    adicionado funcao de logout

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/material.dart';  // Necessário para o uso do BuildContext e AlertDialog
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:provider/provider.dart'; // Importando o Provider
 import 'package:ticke_it/models/user.dart';
+import 'package:ticke_it/providers/user_provider.dart'; // Importando o UserProvider
+import 'package:ticke_it/screens/login_screen.dart'; // Importando a tela de login
 
 class AuthService {
   // FAZER UM ARQUIVO PARA ARMAZENAR A URL BASE
-  static const String _baseUrl = 'http://172.21.192.1:3000';
+  static const String _baseUrl = 'http://172.24.176.1:3000';
 
+  // Função de registro
   static Future<Map<String, dynamic>> register({
     required String name,
     required String email,
@@ -34,7 +39,7 @@ class AuthService {
     }
   }
 
-   static Future<Map<String, dynamic>> login({
+  static Future<Map<String, dynamic>> login({
     required String email,
     required String password,
   }) async {
@@ -56,5 +61,40 @@ class AuthService {
       print(errorBody);
       throw Exception(errorBody['message'] ?? 'Erro no login');
     }
+  }
+
+  static void logout(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Confirmação'),
+          content: const Text('Você tem certeza que deseja sair?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar diálogo
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o diálogo
+                // Logout no UserProvider
+                Provider.of<UserProvider>(context, listen: false).logout();
+                // Navegar para a tela de login
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => const LoginScreen()),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Logout realizado com sucesso!')),
+                );
+              },
+              child: const Text('Sair'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
