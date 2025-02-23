b971caab8fd8d59165c5c5e3e66ab877cd1fe2b6
akiwnl <kaikecarvalho@alunos.utfpr.edu.br>
2025-02-23 17:12:08 -0300

    refactor: remove cart

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -2,23 +2,22 @@ import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget implements PreferredSizeWidget {
   final VoidCallback onMenuPressed;
-  final VoidCallback onCartPressed;
   final VoidCallback onTitlePressed;
 
   const Header({
     Key? key,
     required this.onMenuPressed,
-    required this.onCartPressed,
     required this.onTitlePressed,
   }) : super(key: key);
 
   @override
-  Size get preferredSize => const Size.fromHeight(60.0); // Altura personalizada do AppBar
+  Size get preferredSize =>
+      const Size.fromHeight(60.0); // Altura personalizada do AppBar
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      iconTheme: const IconThemeData(color: Colors.white), 
+      iconTheme: const IconThemeData(color: Colors.white),
       title: GestureDetector(
         onTap: onTitlePressed,
         child: const Text(
@@ -32,19 +31,13 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
         ),
       ),
       centerTitle: true,
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.shopping_cart),
-          iconSize: 28,
-          color: Colors.white,
-          onPressed: onCartPressed,
-        ),
-        const SizedBox(width: 8), // Espaço entre ícones
-      ],
       flexibleSpace: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 0, 0, 0), Color.fromARGB(255, 100, 100, 100)],
+            colors: [
+              Color.fromARGB(255, 0, 0, 0),
+              Color.fromARGB(255, 100, 100, 100)
+            ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
